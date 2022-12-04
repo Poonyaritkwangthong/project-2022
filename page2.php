f1528cd787265fe9274530c2f962e3fb0f4094cc
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2022-12-04 20:34:34 +0700

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page2</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div>
+        <h1>
+            Set Element
+        </h1>
+    </div>
+    <div>
+        <h2>
+            การร่างภาพ คือ การกำหนดโครงสร้างคล่าวๆในภาพนั้นๆ
+        </h2>
+        <div>
+            <p>สมมุติว่าเราจะทำการวาดตัวละครที่เป็นผู้ชายสัก1คน</p>
+            <p>กำหนดสัดส่วนของร่างกายว่าเราต้องการเเบบไหน ตามที่เราต้องการ</p>
+        </dive>
+        <ul class="list-inside ...">
+           <li>ตัวอย่างที่1</li>
+           <!-- ... -->
+        </ul>
+        <ul class="list-inside ...">
+           <li>ตัวอย่างที่2</li>
+           <!-- ... -->
+        </ul>
+    </dive>
+    <div  class="flex space-x-4 ... grid-flow-col-dense">
+  <a class ="bg-slate-500 "href = "index.php">00</a>
+  <a class ="bg-slate-500 "href = "page1.php">01</a>
+  <a class ="bg-slate-500 "href = "page2.php">02</a>
+  <a class ="bg-slate-500 "href = "page3.php">03</a>
+  <a class ="bg-slate-500 "href = "page4.php">04</a>
+  <a class ="bg-slate-500 "href = "page5.php">05</a>
+  <a class ="bg-slate-500 "href = "page6.php">06</a>
+</body>
+</html>
