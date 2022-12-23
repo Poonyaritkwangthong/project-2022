74d092f47720e7464971cad109feb9a89f3fb507
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2022-12-23 10:56:07 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -13,35 +13,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-<body class="bg-gradient-to-r from-indigo-500 ...">
-<div class="navbar flex w-full box-decoration-clone bg-gradient-to-r from-indigo-600/70 to-pink-500/70 text-white px-2 ... ">
-   <div class="logospace-x-10 px-20 items-center text-white text-2xl font-extrabold ...">
-      <h1>Welcome To Drawing</h1>
-    </div> 
-    <div class="flex justify-end w-full space-x-10 px-20">
-    <button class="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 ..."><a href="login.php">Login</a></button>
-    <button class="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 ..."><a href="register.php">Register</a></button>
-   </div>
-</div>
-<div class="text-center text-3xl pt-6 font-extrabold ..."><h2 class="text-white">Program</h2></div>
-<div class="justify-center">
-  <div class="columns-3xs ... blur-none ... scale-100 ... pt-8 ... ">
-   <img class="w-full aspect-video ..." style="background-image: url(...)" src="/6414421006/Project/photo8.jpg" /><button class="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 ... rounded-r-xl ... p-4"><a class="bg--500/50 shadow-lg shadow-cyan-500/50 ... text-2xl text-white" href = "page1.php">การร่างภาพ</a></button>
-   <img class="w-full aspect-video ..." style="background-image: url(...)" src="/6414421006/Project/photo4.jpg" /><button class="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 ... rounded-r-xl ... p-4"><a class="bg-cyan-500/50 shadow-lg shadow-cyan-500/50 ... text-2xl text-white" href = "page2.php">กำหนดรายละเอียด</a></button>
-   <img class="w-full aspect-video ..." style="background-image: url(...)" src="/6414421006/Project/photo3.jpg" /><button class="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 ... rounded-r-xl ... p-4"><a class="bg-cyan-500/50 shadow-lg shadow-cyan-500/50 ... text-2xl text-white" href = "page3.php">ลงรายละเอียด</a></button>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<header>
+  <div class="nav">
+    <ul>
+      <li class="home"><a href="#">HOME</a></li>
+      <li class="tutorials"><a href="#">tutorials</a>
+      <ul>
+        <li><a href="#">Tutorials #1</a></li>
+        <li><a href="#">Tutorials #2</a></li>
+        <li><a href="#">Tutorials #3</a></li>
+      </ul>
+    </li>
+    <li class="about"><a class="active" herf="#">About</a><li>
+    <li class="News"><a href="#">Newsletter</a>
+      <ul>
+        <li><a href="#">News #1</a></li>
+        <li><a href="#">News #1</a></li>
+        <li><a href="#">News #1</a></li>
+      </ul>
+    </li>
+    <li href="Contact"><a>Contact</a></li>
+    </ul>
   </div>
-  <div class="columns-3xs ... blur-none ... scale-100 ... pt-8 ... ">
-   <img class="w-full aspect-video ..." style="background-image: url(...)" src="/6414421006/Project/photo5.jpg" /><button class="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 ... rounded-r-xl ... p-4"><a class="bg-cyan-500/50 shadow-lg shadow-cyan-500/50 ... text-2xl text-white" href = "page4.php">ลงเงา</a></button>
-   <img class="w-full aspect-video ..." style="background-image: url(...)" src="/6414421006/Project/photo6.jpg" /><button class="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 ... rounded-r-xl ... p-4"><a class="bg-cyan-500/50 shadow-lg shadow-cyan-500/50 ... text-2xl text-white" href = "page5.php">ลงสี</a></button>
-   <img class="w-full aspect-video ..." style="background-image: url(...)" src="/6414421006/Project/photo7.jpg" /><button class="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300 ... rounded-r-xl ... p-4"><a class="bg-cyan-500/50 shadow-lg shadow-cyan-500/50 ... text-2xl text-white" href = "page6.php">ตัดเส้น</a></button>
+</header>
+<div class="main">
+  <div class="slide">
+    <figure>
+     <img src="Project/">
+     <img src="Project/">
+     <img src="Project/car1.jpg">
+     <img src="Project/">
+    </figure>
   </div>
-</div>
-<div>
-<div class="flex justify-end ...">
-   <div class="box-content h-100 w-100 p-4 border- ...">
-    <p></p>
-   </div>
-</div>
+  <div class="contant">
+    <h3></h3>
+   
+  </div>
+  <div class="footer">
 
+  </div>
+</div>
 </body>
 </html>
